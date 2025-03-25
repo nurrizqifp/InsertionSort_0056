934fae50b21ab8372d00294737e2a77aaea629dc
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-25 21:35:24 +0700

    Menambahkan perulangangan while dengan boolean true pada void input

--- InsertionSort_0056.cpp
+++ InsertionSort_0056.cpp
@@ -6,7 +6,20 @@ int n; //Membuat variable inputan n
 
 void input()
 {
+while (true)
+    {
+        cout <<"Masukkan Jumlah Data Pada Array : "; // Membuat Inputan jumlah elemen array
+        cin >> n; // memanggil variable inputan n
 
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
 }
 
 void insertionSort()
