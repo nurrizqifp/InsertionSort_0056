3e6516b2e4a91fe6b7e8b5c09edab49df122da3f
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-25 22:05:35 +0700

    Menambahkan Looping dengan j dimulai dari 0 hingga n-1 pada prosedur void display

--- InsertionSort_0056.cpp
+++ InsertionSort_0056.cpp
@@ -63,6 +63,12 @@ void display()
     cout << "=================================" << endl; // Output ke layar
     cout << "Element Array yang telah tersusun " << endl; // Output ke layar
     cout << "=================================" << endl; // Output ke layar
+
+    for (int j = 0; j < n; j++)
+    { // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; // Output ke layar
+    }
+    cout << endl;
 }
 
 int main()
