4d56e8ded9302cd7026a82672dda8492e5d890d5
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-25 21:21:25 +0700

    Membuat kerangka dasar algoritma Insertion Sort

--- InsertionSort_0056.cpp
+++ InsertionSort_0056.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+int arr[20]; //Membuat array dengan panjang data 20
+int n; //Membuat variable inputan n
+
+void input()
+{
+
+}
+
+void insertionSort()
+{ // Procedure Insertionsort
+
+}
+
+int temp; // Membuat variable data temporary atau penyimpanan sementara
+int j, i; // Membuat variable j sebagai penanda
+
+void display()
+{
+
+}
+
+int main()
+{
+
+}
